8f0798c3b42daf7a49d8e22c887cdca51bfe3d5f
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-20 20:18:06 -0700

    tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const newTask = document.getElementById("create-task-form");
+
+newTask.addEventListener("submit", makeNewTask);
+
+const makeNewTask = (e) => {
+  e.preventDefault();
+  const newTaskBody = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskBody.value;
+  const priority = document.querySelector("select");
+
+  const addPriority = (priority) => {
+    if (priority.value === "high") {
+      priority.classList.add("high");
+    } else if (priority.value === "medium") {
+      priority.classList.add("medium");
+    } else if (priority.value === "low") {
+      priority.classList.add("low");
+    }
+  };
+  appendNewTask(newTask);
+  e.target.reset();
+};
+const appendNewTask = (task) => {
+  document.getElementById("tasks").appendChild(task);
+};
